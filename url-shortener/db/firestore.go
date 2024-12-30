23e7b625630adfbe22d3d605f017ac2201d7e694
Kyle Borot <kyleyabraborot@gmail.com>
2024-12-30 09:10:56 -0500

    configured firestore client init

--- url-shortener/db/firestore.go
+++ url-shortener/db/firestore.go
@@ -1,1 +1,35 @@
-package firestore
+package db
+
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	"github.com/kyleborot/golearn/url-shortener/models"
+	"google.golang.org/api/option"
+)
+
+var Client *firestore.Client
+
+func InitializeFirestore() (*firestore.Client, error) {
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "url-shortener-446004", option.WithCredentialsFile("../serviceAccountKey.json"))
+	if err != nil {
+		log.Fatalf("Failed to create Firestore client: %v", err)
+	}
+
+	Client = client
+	return client, nil
+}
+func StoreShortenedURL(ctx context.Context, urlData models.UrlStruct) error {
+	collectionRef := Client.Collection("urls")
+	_, err := collectionRef.Doc(urlData.ID).Set(ctx, map[string]interface{}{
+		"long_url":  urlData.LongURL,
+		"timestamp": urlData.Timestamp,
+	})
+	if err != nil {
+		log.Printf("Failed to store URL: %v", err)
+		return err
+	}
+	return nil
+}
